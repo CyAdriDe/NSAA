5eec81aeaf9fa5162455f4a15d30e9c5a24b5fc0
Adrián Soria Montoya <cyadride@gmail.com>
2023-05-29 19:05:34 +0200

    env parameters added

--- P2/express-app/oidc.js
+++ P2/express-app/oidc.js
@@ -2,8 +2,8 @@ const OpenIDStrategy = require('passport-auth0-openidconnect').Strategy
 
 const oidcStrategy = new OpenIDStrategy({
   domain: 'dev-017jy36e7crwekzm.us.auth0.com',
-  clientID: '5UXcOEtthjlnYmriLVH3GPe6vpr1UOA7',
-  clientSecret: 'p0KTA-k96JA4KSF1PydACIPm7gs06DmDxbxrgZUd9a4UQC9hHfvfw3YGfBxNq4Bx',
+  clientID: process.env.OIDC_CLIENTID,
+  clientSecret: process.env.OIDC_CLIENTSECRET,
   callbackURL: "http://localhost:3000/auth/oidc/callback",
 },
   function (issuer, audience, profile, cb) {
